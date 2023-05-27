4c23313bed3c51bd4bc752b700ef38965a9dc459
Mehwish Iqbal <mehwishned21@gmail.com>
2023-05-27 09:24:52 -0500

    printing list of ingredients

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for (let i=0; i<ingredients.length; i++)
+{ console.log(ingredients[i]);
+}
+let i=0;
+while (i<ingredients.length){
+
+console.log(ingredients[i]);
+i++;
+}
+
+i=ingredients.length-1;
+while (i<=0){
+
+console.log(ingredients[i]);
+i--;
+}
